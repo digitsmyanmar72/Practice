c605a82f3e427f49b8a40b8026632b2fcf254031
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-05-24 16:29:40 +0630

    third

--- scrpt.js
+++ scrpt.js
@@ -2,3 +2,14 @@
 const express = require ("express");
 //create app
 const app = express();
+//define a route for "/api/people" and get request handler
+app.get("/api/people") , function (req,res){
+    const data = [
+        {name:"nay"},
+        {age:41}]
+    return res.status(200).json(data);
+};
+//listen on port 8000
+app.listen(8000,function(){
+    console.log("server is running on port 8000")
+});
