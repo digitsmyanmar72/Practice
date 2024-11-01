b41d2e698c05af5fd10e24aa1fa28b6711f052e9
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-01 21:59:41 +0630

    89

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-#Python Lists/#Python Collections (Arrays)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "c" in x:
+    newlist.append(x)
+
+print(newlist)
