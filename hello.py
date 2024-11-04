8786e8135351a787ea58d8d318e2f822ed22e779
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-04 16:35:05 +0630

    we

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
-#Python / Dictionaries / Remove items
+#Python - Nested Dictionaries
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
 }
 
-
-thisdict.popitem()
-print(thisdict)
+print(myfamily)
