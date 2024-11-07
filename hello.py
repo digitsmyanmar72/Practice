51aba505e239f09a3af901641564088061218e56
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-07 23:03:39 +0630

    rie

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-#Python Function
-
-def myfun():
-    print("hello")
-    
-myfun()
+class myclass:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+nay = myclass("Aung",41)
+print(nay.name)
+print(nay.age)
