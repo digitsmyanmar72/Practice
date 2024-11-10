0907d59fa67a55c7254dd24ee55d03a8271c01ce
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-10 19:15:33 +0630

    ruei

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
-class myclass:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-nay = myclass("Aung",41)
-print(nay.name)
-print(nay.age)
+# Python file handling
+
+f = open("demofile.txt", "r")
+print(f.read())
