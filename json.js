a83ababbc5ca9352a70e64f5c97d1e92858b8bb0
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-12 23:01:47 +0630

    woj

--- json.js
+++
@@ -1,9 +0,0 @@
-//Json string to Json object
-const person1 = '{"name":"nay","age":43}';
-const person2 =JSON.parse(person1)
-
-console.log(person2.name)
-//JS object to Json string
-const p1 = {name:"nay",age:41};
-const p2 = JSON.stringify(p1)
-console.log(p2)
