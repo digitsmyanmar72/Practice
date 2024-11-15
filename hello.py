1acaada110f49a85423dffaf01ce35cf2696b435
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-15 19:55:38 +0630

    ei

--- hello.py
+++
@@ -1,7 +0,0 @@
-import mymodule
-
-mymodule.greeting("Jonathan")
-
-
-a = mymodule.person1["age"]
-print(a)
---
+++ testmodule.py
@@ -0,0 +1,16 @@
+import mymodule
+
+mymodule.greeting("Jonathan")
+
+
+a = mymodule.person1["age"]
+print(a)
+
+
+import platform
+
+x = platform.system()
+print(x)
+
+x = dir(platform)
+print(x)
