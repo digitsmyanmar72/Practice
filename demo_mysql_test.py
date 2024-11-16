e62db5a60fbff9fc890b6af1ace614f005d3df33
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-16 22:43:23 +0630

    w

--- demo_mysql_test.py
+++ demo_mysql_test.py
@@ -3,7 +3,10 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="80008000"
+  password="80008000",
+  
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase2")
