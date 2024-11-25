b1129a9a6887434172206da7c5db4e481f77140a
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-11-25 21:55:49 +0630

    w

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 class Person:
-  def __init__(mysillyobject, name, age):
-    mysillyobject.name = name
-    mysillyobject.age = age
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-  def myfunc(abc):
-    print("Hello my name is " + abc.name)
+  def __str__(self):
+    return f"{self.name}({self.age})"
 
 p1 = Person("John", 36)
-p1.myfunc()
+
+print(p1)
