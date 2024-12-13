ec8eccec63928cd16f86ab8062796c4582bb0d11
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-12-13 21:46:47 +0630

    wt

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-class myclass:
-    def __init__(self,fname,lname):
-        self.firstname = fname
-        self.lastname = lname
-        
-    def myfunction(self):
-        print (self.firstname,self.lastname)
-        
-myobj = myclass("nay","aung")
-print(myobj.firstname)
-myobj.myfunction()
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
