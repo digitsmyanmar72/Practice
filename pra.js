9d1ca583b4c06aabdcbc3a158d56aa4df573e7ec
digitsmyanmar72 <naywinaung522522@gmail.com>
2024-12-17 22:20:06 +0630

    17.12.24

---
+++ pra.js
@@ -0,0 +1,10 @@
+// Create an Object
+const person = {};
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+
+console.log(person)
